04074232ade3b633c0fb31dc21fafa06265a281d
Gaston Nina <gastonnina@gmail.com>
2013-07-11 01:16:51 -0400

    filtrando por un tipo

--- ajax.php
+++ ajax.php
@@ -9,8 +9,25 @@ if (isset($_GET['txtFiltro'])) {
     $q = new BDmysql;
 //echo "<br>--->$basedatos, $host, $user, $pass";
     $q->conectar($basedatos, $host, $user, $pass);
+    if(strlen($txtFiltro)>0){
+        $keyword=$txtFiltro;
+        $fields=array('auto_placa',
+            'auto_modelo',
+            'auto_color',
+            'auto_tipo',
+            'auto_marca',
+            'auto_observacion',
+            'auto_propietario');
+        
+        $fields_str=implode(',',$fields);
+        $sql="SELECT a.*, MATCH ($fields_str) AGAINST ('$keyword') AS relevance
+FROM ma_auto a
+WHERE MATCH ($fields_str) AGAINST ('$keyword')
+ORDER BY relevance DESC;";
+    }else{
+        $sql = "SELECT * FROM ma_auto";
+    }
     
-    $sql = "SELECT * FROM ma_auto";
   
     $q->consulta($sql);
  $res['code']=200;
@@ -23,6 +40,7 @@ if (isset($_GET['txtFiltro'])) {
             'propietario' => $r['auto_propietario'],
             'marca' => $r['auto_marca'],
             'modelo' => $r['auto_modelo'],
+            'tipo' => $r['auto_tipo'],
             'color' => $r['auto_color'],
             'obs' => $r['auto_observacion'],
                 );
